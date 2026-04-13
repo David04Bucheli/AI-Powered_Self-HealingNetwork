d36b3577d6094338960dd72099b200ff1d207bb4
David04Bucheli <bucheli.sebas@gmail.com>
2026-04-13 18:47:27 -0500

    Update device configurations for VyOS routers

--- devices.py
+++ devices.py
@@ -1,11 +1,24 @@
 # devices.py
-router_r1 = {
-    'device_type': 'cisco_ios',
-    'host': '192.168.1.10',  # La IP que le diste en GNS3
+r1 = {
+    'device_type': 'vyos',
+    'host': '192.168.1.1',
     'username': 'admin',
     'password': 'password123',
-    'secret': 'admin_secret', # Si usas enable password
 }
 
-# Puedes crear una lista para que el script recorra varios equipos
-all_devices = [router_r1]
+r2 = {
+    'device_type': 'vyos',
+    'host': '10.0.12.2', # IP del enlace R1-R2
+    'username': 'admin',
+    'password': 'password123',
+}
+
+r3 = {
+    'device_type': 'vyos',
+    'host': '10.0.13.3', # IP del enlace R1-R3
+    'username': 'admin',
+    'password': 'password123',
+}
+
+# Lista maestra que recorrerá el script de monitoreo
+all_devices = [r1, r2, r3]
