e10bc38ab0d6a1f4c059ecf7a5b89857eba5129e
David04Bucheli <bucheli.sebas@gmail.com>
2026-04-13 19:28:36 -0500

    Update device credentials for vyos devices

--- devices.py
+++ devices.py
@@ -2,22 +2,22 @@
 r1 = {
     'device_type': 'vyos',
     'host': '192.168.1.1',
-    'username': 'admin',
-    'password': 'password123',
+    'username': 'vyos',
+    'password': 'vyos',
 }
 
 r2 = {
     'device_type': 'vyos',
     'host': '10.0.12.2', # IP del enlace R1-R2
-    'username': 'admin',
-    'password': 'password123',
+    'username': 'vyos',
+    'password': 'vyos',
 }
 
 r3 = {
     'device_type': 'vyos',
     'host': '10.0.13.3', # IP del enlace R1-R3
-    'username': 'admin',
-    'password': 'password123',
+    'username': 'vyos',
+    'password': 'vyos',
 }
 
 # Lista maestra que recorrerá el script de monitoreo
