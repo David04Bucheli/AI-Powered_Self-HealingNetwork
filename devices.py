4b8af60c7ac07fa2c7a239fb2b568ef9f60c0e00
Keoma Quiroga <keomaquiroga@gmail.com>
2026-04-18 03:18:50 -0500

    DEVICES

--- devices.py
+++ devices.py
@@ -2,21 +2,21 @@
 r1 = {
     'device_type': 'vyos',
     'host': '192.168.1.1',
-    'username': 'vyos',
+    'username': 'admin',
     'password': 'vyos',
 }
 
 r2 = {
     'device_type': 'vyos',
     'host': '10.0.12.2', # IP del enlace R1-R2
-    'username': 'vyos',
+    'username': 'admin',
     'password': 'vyos',
 }
 
 r3 = {
     'device_type': 'vyos',
     'host': '10.0.13.3', # IP del enlace R1-R3
-    'username': 'vyos',
+    'username': 'admin',
     'password': 'vyos',
 }
 
